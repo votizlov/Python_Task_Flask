3aefa53c6f5a4f5e922dcfd7b7769a422538d23c
votizlov <35692043+votizlov@users.noreply.github.com>
2020-04-06 17:16:43 +0300

    im a baboon

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, send_file
 from flask_qrcode import QRcode
-from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
 import pandas as pd
 
 app = Flask(__name__)
@@ -18,27 +16,15 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
-
-@app.route("/qrcode", methods=["GET"])
-def get_qrcode():
-    # please get /qrcode?data=<qrcode_data>
     data = request.args.get("data", "")
-    img = qrcode(data, mode="raw")
-    driver = webdriver.Opera
-    driver.get("http://www.cbr.ru")
-    content = driver.page_source
-    soup = BeautifulSoup(content)
-    for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
-        name = a.find('div', attrs={'class': '_3wU53n'})
-    price = a.find('div', attrs={'class': '_1vC4OE _2rQ-NK'})
-    rating = a.find('div', attrs={'class': 'hGSR34 _2beYZw'})
-    products.append(name.text)
-    prices.append(price.text)
-    ratings.append(rating.text)
-    df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings})
-    df.to_csv('products.csv', index=False, encoding='utf-8')
-    return qrcode(data)
+    tables = pd.read_html('http://cbr.ru/key-indicators/')
+    print(tables)
+    return str(tables)
+
+@app.route("/converter", methods=["GET"])
+def get_qrcode():
+    data = request.args.get("data", "")#1 - usd, 2 - eur
+    tables = pd.read_html('http://cbr.ru/key-indicators/')
+    print()
+    #print(tables)
+    return data
